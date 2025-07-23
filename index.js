1225f71032d556120a4d34e19902eb03018dfbcb
subhash <subhash@subhashs-MacBook-Air.local>
2025-07-23 12:17:50 -0400

    add

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+
 const app = express();
 
+let port = 8080;
 
-
-let port = 3000;
-
-app.listen(port, () => {
-    console.log(`app is listening on port no ${port} `)
-
+app.listen(port, () =>{
+    console.log(`app is listening on port no ${port} `);
 });
+
+app.use((req, res)=>{
+
+    console.log("request recived");
+});
+
+
